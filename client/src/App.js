c847b19614561b0ff8ceb0e1333b11ca67ab6425
Daniel Ale <danielale9291@gmail.com>
2019-10-13 09:45:13 +0100

    Add a react client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Work is underway</p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/groups/1022943091089176/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit our facebook page, this site will be ready soon.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
